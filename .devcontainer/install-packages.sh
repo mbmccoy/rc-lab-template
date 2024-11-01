11207516da5f83ea6582038e326fa96c74b08a55
Michael McCoy <michael.b.mccoy@gmail.com>
2024-11-01 21:11:32 +0000

    Working

---
+++ .devcontainer/install-packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Add commands to install additional packages here.
+
+echo "Running .devcontainer/setup.sh to install additional packages..."
+# Example: apt-get update && apt-get install --no-install-recommends -y vim
+echo "Done."
